863e7a51cba66dab3f985433f050f68f98757650
Ayushi Rani <ayushijsr8@gmail.com>
2025-08-10 15:10:02 +0530

    Add Components

--- src/assets/redux/pasteSlice.jsx
+++ src/assets/redux/pasteSlice.jsx
@@ -12,18 +12,36 @@ export const pasteSlice = createSlice({
   reducers: {
     addToPaste: (state,action) => {
       const paste=action.payload;
+      //if same paste of same of title
+
       state.pastes.push(paste);
       localStorage.setItem("pastes", JSON.stringify(state.pastes));
       toast("Paste created successfully");
       
     },
     updateToPaste: (state,action) => {
-
+      const paste=action.payload;
+      const index=state.pastes.findIndex((item) =>
+        item.id===paste.id)
+      if(index >= 0){
+        state.pastes[index]=paste;
+        localStorage.setItem("pastes",JSON.stringify(state.pastes));
+        toast.success("Paste updated");
+      }
     },
     resetAllPastes: (state, action) => {
-      
+      state.pastes=[];
+      localStorage.removeItem("pastes");
     },
     removeFromPastes:(state,action) => {
+      const pasteId= action.payload;
+      console.lof(pasteId);
+      const index=state.pastes.findIndex((item) => item._id===pasteId);
+      if(index >= 0){
+        state.pastes.splice(index,1);
+        localStorage.setItem("pastes",JSON.stringify(state.pastes));
+        toast.success("Paste deleted");
+      }
 
     }
   },
